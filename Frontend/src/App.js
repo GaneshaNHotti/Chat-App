37f9a80de26ad60eb9e6951b3c550130b7112b3c
Ganesha N Hotti <ganeshahotti5112000@gmail.com>
2025-08-30 22:00:58 +0530

    intiated  the daisyUI for css with tailwind

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,30 +1,3 @@
-import { Switch, Route } from "wouter";
-import { queryClient } from "./lib/queryClient";
-import { QueryClientProvider } from "@tanstack/react-query";
-import { useAuth } from "./cpmponents/hooks/useAuth";
-import LoginForm from "./components/login-form";
-import NotFound from "./components/not-found";
-
-function Router() {
-  const { isAuthenticated, isLoading } = useAuth();
-
-  return (
-    <Switch>
-      {isLoading || !isAuthenticated ? (
-        <Route path="/" component={LoginForm} />
-      ) : (
-        <Route component={NotFound} />
-      )}
-    </Switch>
-  );
+export default function App() {
+  return <div className="text-red-500">Hello World</div>;
 }
-
-function App() {
-  return (
-    <QueryClientProvider client={queryClient}>
-        <Router />
-    </QueryClientProvider>
-  );
-}
-
-export default App;
