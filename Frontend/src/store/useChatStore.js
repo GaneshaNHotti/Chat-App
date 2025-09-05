3b1e8f99bff46ab3bf26a4d2fe55aa987461d54d
Ganesha N Hotti <ganeshahotti5112000@gmail.com>
2025-09-05 19:04:55 +0530

    added chat functionality

--- Frontend/src/store/useChatStore.js
+++ Frontend/src/store/useChatStore.js
@@ -2,7 +2,7 @@ import { create } from "zustand";
 import { axiosInstance } from "../lib/axios.js";
 import toast from "react-hot-toast";
 
-const useChatStore = create((set) => ({
+export const useChatStore = create((set, get) => ({
     messages: [],
     users: [],
     selectedUser: null,
@@ -26,11 +26,22 @@ const useChatStore = create((set) => ({
         set({ isMessagesLoading: true });
         try {
         const res = await axiosInstance.get(`/messages/${userId}`);
+        console.log(res.data);
         set({ messages: res.data });
         } catch (error) {
         toast.error(error.response.data.message);
         } finally {
         set({ isMessagesLoading: false });
+        }
+    },
+
+    sendMessage: async (messageData) => {
+        const { selectedUser, messages } = get();
+        try {
+        const res = await axiosInstance.post(`/messages/send/${selectedUser.id}`, messageData);
+        set({ messages: [...messages, res.data] });
+        } catch (error) {
+        toast.error(error.response.data.message);
         }
     },
 
