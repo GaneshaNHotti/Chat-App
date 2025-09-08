34d4832ffcd2ba9cd92f1bb315cf0f90a0ba5f61
Ganesha N Hotti <ganeshahotti5112000@gmail.com>
2025-09-08 21:48:51 +0530

    added filter in chat users

--- Frontend/src/components/Sidebar.js
+++ Frontend/src/components/Sidebar.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import useChatStore from '../store/useChatStore.js'
 import SidebarSkeleton from './skeletons/SidebarSkeleton.js'
 import { Users } from 'lucide-react';
@@ -8,10 +8,13 @@ const Sidebar = () => {
   const { getUsers, users, selectedUser, setSelectedUser, isUsersLoading } = useChatStore();
 
   const { onlineUsers } = useAuthStore();
+  const [showOnlineOnly, setShowOnlineOnly] = useState(false);
 
   useEffect(() => {
     getUsers();
   }, [getUsers]);
+  
+  const filteredUsers = showOnlineOnly ? users.filter((user) => onlineUsers.includes(user.id)) : users;
 
   if(isUsersLoading) {
     return <SidebarSkeleton />
@@ -25,7 +28,7 @@ const Sidebar = () => {
           <span className="font-medium hidden lg:block">Contacts</span>
         </div>
         {/* TODO: Online filter toggle */}
-        {/* <div className="mt-3 hidden lg:flex items-center gap-2">
+        <div className="mt-3 hidden lg:flex items-center gap-2">
           <label className="cursor-pointer flex items-center gap-2">
             <input
               type="checkbox"
@@ -36,10 +39,10 @@ const Sidebar = () => {
             <span className="text-sm">Show online only</span>
           </label>
           <span className="text-xs text-zinc-500">({onlineUsers.length - 1} online)</span>
-        </div> */}
+        </div>
       </div>
       <div className="overflow-y-auto w-full py-3">
-        {users.map((user) => (
+        {filteredUsers.map((user) => (
           <button
             key={user.id}
             onClick={() => setSelectedUser(user)}
@@ -73,9 +76,9 @@ const Sidebar = () => {
           </button>
         ))}
 
-        {/* {filteredUsers.length === 0 && (
+        {filteredUsers.length === 0 && (
           <div className="text-center text-zinc-500 py-4">No online users</div>
-        )} */}
+        )}
       </div>
     </aside>
   );
